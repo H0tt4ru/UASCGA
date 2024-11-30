442539e42a9c01f2b35b6a7ee33b3397d4a16f90
Nathan Angelo Stenlie <stenlieangelonathan@gmail.com>
2024-11-30 16:26:08 +0700

    Gun Finished without sound

--- Assets/StarterAssets/FirstPersonController/Scripts/ShootController.cs
+++ Assets/StarterAssets/FirstPersonController/Scripts/ShootController.cs
@@ -1,74 +1,153 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using StarterAssets;
 using TMPro;
 
 public class ShootController : MonoBehaviour
 {
     public Camera playerCamera;
-    public float shootingRange = 100f;
-    public float fireRate = 0.1f; // Time between shots
-    private float nextTimeToFire = 0f;
-    private int shotsFired = 0;
-    private int maxShots = 30;
-    public TextMeshProUGUI ammoText;
+    [SerializeField]
+    private bool AddBulletSpread = true;
+    [SerializeField]
+    private Vector3 BulletSpreadVariance = new Vector3(0.1f, 0.1f, 0.1f);
+    [SerializeField]
+    private ParticleSystem ShootingSystem;
+    [SerializeField]
+    private Transform BulletSpawnPoint;
+    [SerializeField]
+    private ParticleSystem ImpactParticleSystem;
+    [SerializeField]
+    private TrailRenderer BulletTrail;
+    [SerializeField]
+    private float ShootDelay = 0.5f;
+    [SerializeField]
+    private LayerMask Mask;
+    [SerializeField]
+    private float BulletSpeed = 100;
+    private int Ammo = 30;
+    private int MaxAmmo = 30;
+    private int ReserveAmmo = 90;
+    private bool Reloading = false;
+    private float ReloadTime = 1.5f;
+    public TextMeshProUGUI AmmoText;
+    public TextMeshProUGUI ReserveAmmoText;
 
-    void Start()
+    private Animator Animator;
+    private float LastShootTime;
+
+    private void Awake()
     {
-        if (playerCamera == null)
-        {
-            playerCamera = Camera.main;
-        }
+        Animator = GetComponent<Animator>();
+        ShootingSystem = transform.Find("MuzzleFlash01").GetComponent<ParticleSystem>();
+        UpdateAmmoUI();
     }
 
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            Reload();
-        }
-
-        if (Input.GetMouseButton(0) && Time.time >= nextTimeToFire)
-        {
-            if (shotsFired >= maxShots)
-            {
-                Debug.Log("Out of ammo! Press 'R' to reload.");
-                return;
-            }
-
-            nextTimeToFire = Time.time + fireRate;
+    private void Update() {
+        if (Input.GetMouseButton(0) && !Reloading) {
             Shoot();
-            MuzzleFlash();
-            shotsFired++;
+        } else {
+            ShootingSystem.Stop();
         }
 
-        ammoText.text = (maxShots - shotsFired).ToString("D2") + "/30";
+        if (Input.GetKeyDown(KeyCode.R) && !Reloading) {
+            StartCoroutine(Reload());
+        }
     }
 
-    void Shoot()
+    private void Shoot()
     {
-        RaycastHit hit;
-        if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out hit, shootingRange))
-        {
-            Debug.Log(hit.transform.name);
-            Enemy enemy = hit.transform.GetComponent<Enemy>();
+        if (Ammo <= 0) {
+            ShootingSystem.Stop();
+            return;
+        }
 
-            if (enemy != null)
-            {
-                enemy.OnHit(hit.point, playerCamera.transform.forward);
+        ShootingSystem.Play();
+        if (LastShootTime + ShootDelay < Time.time) {
+            Animator.SetBool("IsShooting", true);
+            Vector3 direction = GetDirection();
+        
+            if (Physics.Raycast(BulletSpawnPoint.position, direction, out RaycastHit hit, float.MaxValue, Mask)) {
+                TrailRenderer trail = Instantiate(BulletTrail, BulletSpawnPoint.position, Quaternion.identity);
+                StartCoroutine(SpawnTrail(trail, hit.point, hit.normal, true));
+                Debug.Log(hit.transform.name);
+                Enemy enemy = hit.transform.GetComponent<Enemy>();
+
+                if (enemy != null)
+                {
+                    enemy.OnHit(hit.point, playerCamera.transform.forward);
+                }
+            } else {
+                TrailRenderer trail = Instantiate(BulletTrail, BulletSpawnPoint.position, Quaternion.identity);
+                StartCoroutine(SpawnTrail(trail, BulletSpawnPoint.position + direction * 100, Vector3.zero, false));
             }
+
+            LastShootTime = Time.time;
+            Ammo--;
+            UpdateAmmoUI();
         }
     }
 
-    void MuzzleFlash()
+    private Vector3 GetDirection()
     {
-        // Implement muzzle flash logic here
+        Vector3 direction = transform.forward;
+        if (AddBulletSpread) {
+            direction += new Vector3(
+                Random.Range(-BulletSpreadVariance.x, BulletSpreadVariance.x),
+                Random.Range(-BulletSpreadVariance.y, BulletSpreadVariance.y),
+                Random.Range(-BulletSpreadVariance.z, BulletSpreadVariance.z)
+            );
+            direction.Normalize();
+        }
+        return direction;
     }
 
-    void Reload()
+    private IEnumerator SpawnTrail(TrailRenderer trail, Vector3 hitPoint, Vector3 hitNormal, bool madeImpact)
     {
-        shotsFired = 0;
-        Debug.Log("Reloaded!");
+        Vector3 startPosition = trail.transform.position;
+        float distance = Vector3.Distance(startPosition, hitPoint);
+        float remainingDistance = distance;
+
+        while (remainingDistance > 0) {
+            trail.transform.position = Vector3.Lerp(startPosition, hitPoint, 1 - (remainingDistance / distance));
+            remainingDistance -= BulletSpeed * Time.deltaTime;
+            yield return null;
+        }
+
+        Animator.SetBool("IsShooting", false);
+        trail.transform.position = hitPoint;
+        if (madeImpact) {
+            Instantiate(ImpactParticleSystem, hitPoint, Quaternion.LookRotation(hitNormal));
+        }
+
+        Destroy(trail.gameObject, trail.time);
+    }
+
+    private IEnumerator Reload()
+    {
+        if (Ammo == MaxAmmo || ReserveAmmo <= 0) {
+            yield break;
+        }
+
+        Reloading = true;
+        Animator.SetBool("IsReloading", true);
+        yield return new WaitForSeconds(ReloadTime);
+
+        int ammoToReload = MaxAmmo - Ammo;
+        if (ReserveAmmo >= ammoToReload) {
+            Ammo += ammoToReload;
+            ReserveAmmo -= ammoToReload;
+        } else {
+            Ammo += ReserveAmmo;
+            ReserveAmmo = 0;
+        }
+
+        Reloading = false;
+        Animator.SetBool("IsReloading", false);
+        UpdateAmmoUI();
+    }
+
+    private void UpdateAmmoUI()
+    {
+        AmmoText.text = Ammo.ToString();
+        ReserveAmmoText.text = ReserveAmmo.ToString();
     }
 }
